5be2b8bb43a4f4799400f5817fba40d8d628a120
mbekins <milobekins@gmail.com>
2018-05-29 20:35:28 -0700

    First Push

---
+++ portfolio/js/javascript.js
@@ -0,0 +1,17 @@
+document.getElementById("button1").onclick = function() {
+    document.getElementById("box").style.height = "250px"
+};
+
+document.getElementById("button2").onclick = function() {
+    document.getElementById("box").style.backgroundColor = "blue"
+};
+
+document.getElementById("button3").onclick = function() {
+    document.getElementById("box").style.opacity = "0.1"
+};
+
+document.getElementById("button4").onclick = function() {
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+};
